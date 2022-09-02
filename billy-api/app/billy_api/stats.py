1e0181a515f09e79cd10f5b6c915b2da4a0984cb
adriandolha <adriandolha@yahoo.com>
2022-09-02 17:19:00 +0300

    Remove some debug lines

--- billy-api/app/billy_api/stats.py
+++ billy-api/app/billy_api/stats.py
@@ -17,7 +17,7 @@ class StatsApi:
         exclude_query = ' and '.join([f"~desc.str.contains('{exp}')" for exp in exclude_list])
         LOGGER.info(exclude_query)
         _df = get_data_df().query(f'suma < 0 and {exclude_query}')
-        LOGGER.debug(_df.to_string())
+        # LOGGER.debug(_df.to_string())
         _df['year'] = pd.DatetimeIndex(_df['date']).year
         _df['month'] = pd.DatetimeIndex(_df['date']).month
         _df = _df.groupby(['year', 'month'], as_index=False).sum()
@@ -28,7 +28,7 @@ class StatsApi:
 
     def avg_expenses_per_category(self) -> dict:
         _df = get_data_df().query('suma < 0')
-        LOGGER.debug(_df.to_string())
+        # LOGGER.debug(_df.to_string())
         _df['year'] = pd.DatetimeIndex(_df['date']).year
         _df['month'] = pd.DatetimeIndex(_df['date']).month
         _df = _df.groupby(['category','year', 'month'], as_index=False).sum()
@@ -41,7 +41,7 @@ class StatsApi:
 
     def expenses_per_month_and_category(self) -> dict:
         _df = get_data_df().query('suma < 0')
-        LOGGER.debug(_df.to_string())
+        # LOGGER.debug(_df.to_string())
         _df['year'] = pd.DatetimeIndex(_df['date']).year
         _df['month'] = pd.DatetimeIndex(_df['date']).month
         _df = _df.groupby(['category', 'year', 'month'], as_index=False).sum()
